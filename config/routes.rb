15f21276821b58951d69f1e186d1603eba4cbcd5
Harry Gilling <hwgilling@gmail.com>
2014-11-02 10:59:49 -0800

    remove root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'students#index'
+  #root 'students#index'
   resources :students
 
   # The priority is based upon order of creation: first created -> highest priority.
